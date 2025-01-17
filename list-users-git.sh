60062feb20d3e3c808fd6a4c0b1065f67901c547
KGJUP12 <88571326+KGJUP12@users.noreply.github.com>
2025-01-17 12:57:51 +0530

    Updated

--- list-users-git.sh
+++ list-users-git.sh
@@ -50,7 +50,7 @@ function list_users_with_read_access {
 #Function to let user know 
 function helper(){
     expected_cmd_args=2
-    if($# -ne expected_cmd_args); then
+    if($# -ne $expected_cmd_args); then
         echo "Please provide required Parameters"
     fi
     }
